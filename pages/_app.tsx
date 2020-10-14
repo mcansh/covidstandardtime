54d35d002711895eb48c11fe4af989b672bd6b51
Logan McAnsh <logan@mcan.sh>
2020-10-14 09:55:01 -0400

    chore: add fathom analytics
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,22 @@ import 'styles/index.css';
 
 import React from 'react';
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import * as Fathom from 'fathom-client';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview();
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  React.useEffect(() => {
+    Fathom.load('KACCORGG', {
+      excludedDomains: ['localhost'],
+      url: 'https://kiwi.mcan.sh/script.js',
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
+};
 
 export default App;
