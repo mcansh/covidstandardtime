81cb437d99a1a29a3bf3750f9e04e2d5b5486bab
Logan McAnsh <logan@mcan.sh>
2020-10-14 10:03:28 -0400

    chore: save service worker to public dir
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const withPWA = require('next-pwa');
 module.exports = withPWA(
   withSourceMaps({
     pwa: {
+      dest: 'public',
       disable: process.env.NODE_ENV === 'development',
       register: true,
     },
-
     crossOrigin: 'anonymous',
     target: 'serverless',
     experimental: {
